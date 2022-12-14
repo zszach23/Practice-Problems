08bff4a7219abd9b885365d9644738eb6fb3a03b
Zach S <zszach23@gmail.com>
2022-12-14 08:37:50 -0500

    Added Queue to tree

---
+++ Generic Data Structures/Queue.java
@@ -0,0 +1,15 @@
+// Zach Sally
+// December 14, 2022
+
+// ================
+// |  Queue.java  |
+// ================
+
+// My implementation of a Queue using LinkedList
+
+import java.util.*;
+
+public class Queue<AnyType extends Comparable<AnyType>>
+{
+    
+}
